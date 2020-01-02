0bf28e6351f11cbfd6d0666250d8f7aae02c675a
RodPSouza <rod.p.souza@hotmail.com>
2020-01-01 23:29:54 -0300

    Aula-25

--- src/main/java/com/nelioalves/curso_mc/domain/Pedido.java
+++ src/main/java/com/nelioalves/curso_mc/domain/Pedido.java
@@ -3,6 +3,8 @@ package com.nelioalves.curso_mc.domain;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Pedido implements Serializable {
@@ -24,6 +26,8 @@ public class Pedido implements Serializable {
     @ManyToOne
     @JoinColumn(name="endereco_de_entrega_id")
     private Endereco enderecoDeEntrega;
+
+    private Set<ItemPedido> itens = new HashSet<>();
 
     public Pedido(){
     }
@@ -74,6 +78,14 @@ public class Pedido implements Serializable {
 
     public void setEnderecoDeEntrega(Endereco enderecoDeEntrega) {
         this.enderecoDeEntrega = enderecoDeEntrega;
+    }
+
+    public Set<ItemPedido> getItens() {
+        return itens;
+    }
+
+    public void setItens(Set<ItemPedido> itens) {
+        this.itens = itens;
     }
 
     @Override
