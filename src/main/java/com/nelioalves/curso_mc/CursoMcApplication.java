7118bc5764539323839e352a1a8d3860be3cdb5c
RodPSouza <rod.p.souza@hotmail.com>
2020-01-01 22:31:36 -0300

    Aula-24

--- src/main/java/com/nelioalves/curso_mc/CursoMcApplication.java
+++ src/main/java/com/nelioalves/curso_mc/CursoMcApplication.java
@@ -91,13 +91,13 @@ public class CursoMcApplication implements CommandLineRunner {
 		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
 		ped1.setPagamento(pagto1);
 
-		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped1, sdf.parse("20/10/2017 00:00"), null);
-		ped1.setPagamento(pagto2);
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
 
 		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
 
-		pedidoRepository.save(Arrays.asList(ped1, ped2));
-		//pagamentoRepository.save(Arrays.asList(pagto1, pagto2));
+		//pedidoRepository.save(Arrays.asList(ped1, ped2));
+		pagamentoRepository.save(Arrays.asList(pagto1, pagto2));
 		
 	}
 }
