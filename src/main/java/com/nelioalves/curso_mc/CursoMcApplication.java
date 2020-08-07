ec069f8fc3a6f338806b103cab107e9f9618cf85
RodPSouza <rod.p.souza@hotmail.com>
2020-08-07 00:36:10 -0300

    Aula - 57 - Criando o profile de desenvolvimento.

--- src/main/java/com/nelioalves/curso_mc/CursoMcApplication.java
+++ src/main/java/com/nelioalves/curso_mc/CursoMcApplication.java
@@ -1,31 +1,18 @@
 package com.nelioalves.curso_mc;
 
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-
-import com.nelioalves.curso_mc.domain.*;
-import com.nelioalves.curso_mc.enums.EstadoPagamento;
-import com.nelioalves.curso_mc.repositories.*;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.nelioalves.curso_mc.enums.TipoCliente;
-
 @SpringBootApplication
 public class CursoMcApplication implements CommandLineRunner {
-	
 
-
-	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoMcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		
-
 	}
+
 }
