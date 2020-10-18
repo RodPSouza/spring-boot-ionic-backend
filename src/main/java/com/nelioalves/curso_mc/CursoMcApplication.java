f2c932673cae2a978535db038ed29982ef13da80
RodPSouza <rod.p.souza@hotmail.com>
2020-10-17 22:46:18 -0300

    Aula 84 - Salvando o primeiro arquivo no S3

--- src/main/java/com/nelioalves/curso_mc/CursoMcApplication.java
+++ src/main/java/com/nelioalves/curso_mc/CursoMcApplication.java
@@ -1,5 +1,7 @@
 package com.nelioalves.curso_mc;
 
+import com.nelioalves.curso_mc.services.S3Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,12 +9,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CursoMcApplication implements CommandLineRunner {
 
+	@Autowired
+	private S3Service s3Service;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursoMcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+		s3Service.uploadFile("C:\\Users\\rodps\\OneDrive\\Imagens\\jJG6Jow.jpg");
 	}
 
 }
